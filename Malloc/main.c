0c501bc459a7df5d8dd6ca8b01009af29aa8be3b
JPSS14 <jp_scarabelli@hotmail.com>
2020-05-09 19:47:00 -0300

    Malloc 1.0

---
+++ Malloc/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program is a malloc exemple, read 3 integers numbers and do a dinamic save, and print all values */
+
+int main(int argc, char *argv[]){
+	int *t,i;
+	t = malloc(sizeof(int)*3);
+	for(i=0;i<3;i++){
+		printf("Enter a integer number: ");
+		scanf("%d",&t[i]);
+	}
+	for(i=0;i<3;i++){
+		printf("\n%d",t[i]);
+	}
+	free(*t);
+	return 0;
+}
