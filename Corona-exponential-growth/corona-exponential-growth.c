ab4aafeba13ffc65889df6e512cb76c4541006f7
JPSS14 <jp_scarabelli@hotmail.com>
2020-05-11 23:46:09 -0300

    Corona exponential growth UPDATE 1: Add n_exposed regress

--- Corona-exponential-growth/corona-exponential-growth.c
+++ Corona-exponential-growth/corona-exponential-growth.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-	float n_exposed=18, chance_contagion=0.028, n_cases=1, result=0;
+	float n_exposed=30, chance_contagion=0.002, n_cases=1;
 	int i;
-	for (i=0;i<30;i++){
-		n_cases += n_cases*(1+n_exposed*chance_contagion);
-		printf("Dia %d = %.0f\n",i+1,n_cases);
+	for (i=0;i<75;i++){
+		if(i%4==0){
+			n_cases += n_cases*(1+n_exposed*chance_contagion);
+			n_exposed-=n_exposed*0.028;
+		}
+		printf("Dia %d  = %.0f \n",i+1,n_cases);
+	
 	}
 	printf("\n\nTotal = %.0f\n",n_cases);
 	return 0;
